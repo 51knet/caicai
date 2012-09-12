9d6c603b81f5a8c6f8c9b4da62a3ffc2da05e879
jihao <jacky.jihao@gmail.com>
2012-09-13 00:32:17 +0800

    add student entity

---
+++ ccweb/src/main/java/com/knet51/ccweb/jpa/entities/Student.java
@@ -0,0 +1,78 @@
+package com.knet51.ccweb.jpa.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="user_id")
+	private User user; // identify the basic user information
+	//Student FOREIGN KEY (`user_id`) REFERENCES `usr` (`id`)
+	
+	private String email;
+	private Integer role;
+	private String college;
+	private String junior_high_school;
+	private String senior_high_school;
+	private String primary_school;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getRole() {
+		return role;
+	}
+	public void setRole(Integer role) {
+		this.role = role;
+	}
+	public String getCollege() {
+		return college;
+	}
+	public void setCollege(String college) {
+		this.college = college;
+	}
+	public String getJunior_high_school() {
+		return junior_high_school;
+	}
+	public void setJunior_high_school(String junior_high_school) {
+		this.junior_high_school = junior_high_school;
+	}
+	public String getSenior_high_school() {
+		return senior_high_school;
+	}
+	public void setSenior_high_school(String senior_high_school) {
+		this.senior_high_school = senior_high_school;
+	}
+	public String getPrimary_school() {
+		return primary_school;
+	}
+	public void setPrimary_school(String primary_school) {
+		this.primary_school = primary_school;
+	}
+}
