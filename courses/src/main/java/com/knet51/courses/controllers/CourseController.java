176ab14b67855d338ab25ec6e0f3dab81e0d2180
zhangmin <971903103@qq.com>
2012-12-07 16:14:36 +0800

    add the courseBeans...

--- courses/src/main/java/com/knet51/courses/controllers/CourseController.java
+++ courses/src/main/java/com/knet51/courses/controllers/CourseController.java
@@ -1,11 +1,15 @@
 package com.knet51.courses.controllers;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.knet51.courses.beans.TeacherCourseBeans;
 import com.knet51.courses.jpa.services.TeacherCourseService;
 
 @Controller
@@ -16,7 +20,9 @@ public class CourseController {
 	private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
 	
 	@RequestMapping(value="/course/list")
-	public String showAllCourse(){
+	public String showAllCourse(Model model){
+		List<TeacherCourseBeans> tcBeanList = courseService.getAllTeacherCourseBeans();
+		model.addAttribute("courseList", tcBeanList);
 		return null;
 	}
 
