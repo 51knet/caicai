0a4b44535e6d9bb69fa428259a9c9b8462f877b4
liubaixin <liubaixin88@163.com>
2013-01-05 13:04:53 +0800

    modify commit

---
+++ data-access-layer/src/main/java/com/knet51/ccweb/jpa/entities/teacher/UserCourse.java
@@ -0,0 +1,54 @@
+package com.knet51.ccweb.jpa.entities.teacher;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.knet51.ccweb.jpa.entities.AbstractEntity;
+import com.knet51.ccweb.jpa.entities.User;
+@Entity
+@Table(name="usercourse")
+public class UserCourse extends AbstractEntity {
+	private Long mark;
+	private String commentDesc;
+	private String commentDate;
+	private Long userid;
+	private Long teachercourseid;
+	public Long getTeachercourseid() {
+		return teachercourseid;
+	}
+	public void setTeachercourseid(Long teachercourseid) {
+		this.teachercourseid = teachercourseid;
+	}
+	public Long getUserid() {
+		return userid;
+	}
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
+	public Long getMark() {
+		return mark;
+	}
+	public void setMark(Long mark) {
+		this.mark = mark;
+	}
+	public String getCommentDesc() {
+		return commentDesc;
+	}
+	public void setCommentDesc(String commentDesc) {
+		this.commentDesc = commentDesc;
+	}
+	public String getCommentDate() {
+		return commentDate;
+	}
+	public void setCommentDate(String commentDate) {
+		this.commentDate = commentDate;
+	}
+	public UserCourse(){
+		super();
+	}
+	
+	
+	
+	
+}
