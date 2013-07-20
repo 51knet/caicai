1864d02bc6a4625c16084f06a0edb2b15768aa82
Kay Shan <shince@hotmail.com>
2013-07-20 14:36:42 +0800

    add varity description method

--- ccweb/src/main/java/com/knet51/ccweb/beans/TrendsBeans.java
+++ ccweb/src/main/java/com/knet51/ccweb/beans/TrendsBeans.java
@@ -1,6 +1,7 @@
 package com.knet51.ccweb.beans;
 
 import java.util.List;
+import java.util.Map;
 
 import com.knet51.ccweb.jpa.entities.Comment;
 import com.knet51.ccweb.jpa.entities.timeline.Trends;
@@ -9,12 +10,15 @@ public class TrendsBeans {
 	private Trends trend;
 	private List<Comment> commentList;
 	private Long commentCount;
+	private String varityDescription;
+	private static Map<String, String> varityMap;
 
 	public Trends getTrend() {
 		return trend;
 	}
 	public void setTrend(Trends trend) {
 		this.trend = trend;
+		this.varityDescription = parseVarity(trend);
 	}
 	public List<Comment> getCommentList() {
 		return commentList;
@@ -29,8 +33,22 @@ public class TrendsBeans {
 	public void setCommentCount(Long commentCount) {
 		this.commentCount = commentCount;
 	}
+	public String getVarityDescription() {
+		return varityDescription;
+	}
+	public void setVarityDescription(String varityDescription) {
+		this.varityDescription = varityDescription;
+	}
 	public TrendsBeans() {
 		super();
+		varityMap.put("announcement", "一篇公告");
+		varityMap.put("courseresource", "一份资源");
+		varityMap.put("course", "一节课程");
+	}
+	
+	private String parseVarity(Trends trend){
+		String varity = trend.getVariety();
+		return varityMap.get(varity);
 	}
 	
 }
